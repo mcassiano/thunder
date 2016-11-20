b09c551addb513a7921a0a88509596a5479c5b3c
Matheus C. Candido <matheus@mcassiano.com>
2016-11-20 21:43:00 -0200

    reverting: a byte var cannot be assigned an int

--- src/me/cassiano/thunder/Parser.java
+++ src/me/cassiano/thunder/Parser.java
@@ -167,8 +167,7 @@ public class Parser {
                 SymbolType type = exp_value_const();
 
                 if (type != tempType)
-                    if (!(tempType == SymbolType.INTEGER && type == SymbolType.BYTE ||
-                            tempType == SymbolType.BYTE && type == SymbolType.INTEGER))
+                    if (!(tempType == SymbolType.INTEGER && type == SymbolType.BYTE))
                         throw new IncompatibleTypes(LexicalAnalyzer.get().getLineNumber(), tempType.toString(), type.toString());
             }
 
